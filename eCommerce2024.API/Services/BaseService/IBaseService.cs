8d51a307bc7b9d00e1c000aceb556c5ce29a6901
Anita Dautovic <anita.dautovic@iea2.tech>
2024-04-23 16:58:30 +0200

    base controller and base service

---
+++ eCommerce2024.API/Services/BaseService/IBaseService.cs
@@ -0,0 +1,11 @@
+﻿namespace eCommerce2024.API
+{
+    public interface IBaseService<TDatabase, TModel, TUpdate, TInsert>
+    {
+        public List<TModel> GetAll();
+        public TModel GetById(int id);
+        public TModel Update(int id, TUpdate update);
+        public TModel Insert(TInsert insert);
+        public bool Delete(int id);
+    }
+}
